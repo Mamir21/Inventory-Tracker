f9e98068569b795b1f4103f196ef7b8c44fc9ecc
Muhammad Amir <umairamir1127@gmail.com>
2024-08-02 22:59:34 -0400

    error

--- firebase.js
+++ firebase.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: "inventory-tracker-863f6",
-  storageBucket: process.senv.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
